ba2fa2d5bcdd277de3efab7a2c5dbde4279ee315
Maia <marcelomaias@gmail.com>
2023-08-05 23:23:38 -0300

    Initial

---
+++ configs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd ~/debian12-i3
+
+cp -r Wallpapers/ ~/Pictures
+cp -r config/alacritty/ ~/.config
+cp -r config/i3/ ~/.config
+cp -r config/i3status/ ~/.config
+cp -r config/mpv/ ~/.config
+cp -r config/picom/ ~/.config
+cp -r config/polybar/ ~/.config
+cp -r config/rofi/ ~/.config
---
+++ install.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# xorg display server installation
+sudo apt install -y xorg xbacklight xbindkeys xvkbd xinput xorg-dev
+
+sudo apt install -y intel-microcode
+
+sudo apt install -y lxappearance
+
+# File Manager (eg. thunar,krusader)
+sudo apt install -y pcmanfm
+
+# Network File Tools/System Events
+sudo apt install -y dialog mtools dosfstools avahi-daemon acpi acpid gvfs-backends
+
+sudo systemctl enable avahi-daemon
+sudo systemctl enable acpid
+
+# Terminal (eg. terminator,kitty,xfce4-terminal,tilix)
+sudo apt install -y kitty
+
+# Sound packages
+sudo apt install -y pulseaudio alsa-utils pavucontrol volumeicon-alsa
+
+# Neofetch/HTOP
+sudo apt install -y neofetch htop
+
+# Printing and bluetooth (if needed)
+sudo apt install -y cups
+sudo apt install -y bluez blueman
+
+sudo systemctl enable bluetooth
+sudo systemctl enable cups
+
+# Browser Installation (eg. chromium)
+sudo apt install -y firefox-esr
+
+# Desktop background browser/handler
+#sudo apt install -y feh
+# feh --bg-fill /path/to/directory 
+# example if you want to use in autostart.sh for i3-gaps
+# sudo apt install -y nitrogen
+sudo apt install -y feh
+
+# Lightdm
+sudo apt install -y lightdm slick-greeter lightdm-settings
+sudo systemctl enable lightdm
+
+# Create folders in user directory (eg. Documents,Downloads,etc.)
+xdg-user-dirs-update
+
+# i3-wm
+sudo apt install -y i3-wm
+
+# Packages needed i3-wm after installation
+sudo apt install -y dmenu sxhkd rofi dunst libnotify-bin picom unzip simple-scan
+
+
+source ~/debian12-i3/configs.sh
+
+
+sudo apt autoremove
+
+printf "\e[1;32mAll done! You can now reboot.\e[0m\n"
